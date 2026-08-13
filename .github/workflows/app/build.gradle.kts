6f68b80d83aaf2bbd01a19d354f0ba41aae0badc
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 12:06:18 +0300

    Create app/build.gradle.kts

---
+++ .github/workflows/app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.moonanime.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.moonanime.app"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+
+    val composeBom =
+        platform("androidx.compose:compose-bom:2026.06.00")
+
+    implementation(composeBom)
+
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-compose:1.12.0")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-compose:2.9.2"
+    )
+
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
+    )
+
+    implementation(
+        "com.squareup.okhttp3:okhttp:5.1.0"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+}
