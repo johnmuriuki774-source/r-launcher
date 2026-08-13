2478cfbd408795e40b6e93fdbfd8c080fa176a2f
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 12:13:11 +0300

    Update build.gradle.kts

--- .github/workflows/app/build.gradle.kts
+++ .github/workflows/app/build.gradle.kts
@@ -55,6 +55,14 @@ dependencies {
         "com.squareup.okhttp3:okhttp:5.1.0"
     )
 
+    implementation(
+        "io.coil-kt.coil3:coil-compose:3.3.0"
+    )
+
+    implementation(
+        "io.coil-kt.coil3:coil-network-okhttp:3.3.0"
+    )
+
     debugImplementation(
         "androidx.compose.ui:ui-tooling"
     )
